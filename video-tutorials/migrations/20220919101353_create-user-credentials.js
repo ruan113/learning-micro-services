80c8979df9e6e1a634d59ceeede2ec4f0ebec0b9
Ruan <ruanbento@gmail.com>
2022-09-19 08:28:04 -0300

    feat: create user_credentials table migration

---
+++ video-tutorials/migrations/20220919101353_create-user-credentials.js
@@ -0,0 +1,10 @@
+exports.up = function up(knex) {
+  return knex.schema.createTable('user_credentials', table => {
+    table.string('id').primary()
+    table.string('email').notNullable()
+    table.string('password_hash').notNullable()
+    table.index('email')
+  })
+}
+
+exports.down = knex => knex.schema.dropTable('user_credentials')
